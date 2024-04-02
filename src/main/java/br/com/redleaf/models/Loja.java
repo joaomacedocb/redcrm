57fd05ccf7f355918eba32faac531562cacce72f
joaomacedocb <joaomacedocb@gmail.com>
2024-04-02 07:32:07 -0300

    Completing class Loja with constructor and getters and setters.

--- src/main/java/br/com/redleaf/models/Loja.java
+++ src/main/java/br/com/redleaf/models/Loja.java
@@ -6,4 +6,43 @@ public class Loja {
     private String location;
     private String webSite;
     private boolean isEcommerce;
+
+    public Loja(String name, String location, String webSite, boolean isEcommerce) {
+        this.name = name;
+        this.location = location;
+        this.webSite = webSite;
+        this.isEcommerce = isEcommerce;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getWebSite() {
+        return webSite;
+    }
+
+    public void setWebSite(String webSite) {
+        this.webSite = webSite;
+    }
+
+    public boolean isEcommerce() {
+        return isEcommerce;
+    }
+
+    public void setEcommerce(boolean ecommerce) {
+        isEcommerce = ecommerce;
+    }
 }
